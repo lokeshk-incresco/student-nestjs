9068760baf1236299e80da2ebf8bf9afa9227df7
lokeshk-incresco <lokesh.kasi@increscotech.com>
2023-08-31 15:34:46 +0530

    CICD setup using Github actions with ECS and ECR

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.MONGODB_URI)
 @Module({
   imports: [
     StudentModule,
     MongooseModule.forRoot(
-      'mongodb+srv://lokesh:lokesh123@cluster0.z8mbmza.mongodb.net',
+      process.env.MONGODB_URI||"mongodb+srv://lokesh:lokesh123@cluster0.z8mbmza.mongodb.net",
     ),
   ],
   controllers: [AppController],
